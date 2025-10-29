3098f73585f1d1099c139e0bed270d7f89fbc4ad
afields727 <afiel0072@launchpadphilly.org>
2025-10-29 11:28:48 -0400

    updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "70f7213f468176a0"; }
+/******/ 		__webpack_require__.h = function() { return "cae1b1c2144f3cc0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
